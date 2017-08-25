e47b99b725d8ff642f569fadd0420e21195a1633
Staff <staff@Radostinas-MacBook-Pro.local>
2017-08-25 15:03:48 -0400

    added agency landing page

--- jquery/script.js
+++ jquery/script.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $(".tab").click(function(){
       $(".advanced-filter").slideToggle();
     });
+
+    $('.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+
+      $('.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+
 });
